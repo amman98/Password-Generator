dc5ea3ad948ff0bb9deebae4853cbdbec20b884b
Amman Nega <ammannega@gmail.com>
2023-03-29 20:39:10 -0700

    added function to confirm characters to include

--- script.js
+++ script.js
@@ -28,17 +28,70 @@ function writePassword() {
 var lowerCaseChars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upperCaseChars = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var numericChars = ['1','2','3','4','5','6','7','8','9','0'];
-var specialChars = [' ', '!', '"', '#', '$', '%', '&', ' ', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ' ', ']', '^', '_', '`', '{', '|', '}', '~'];
+var specialChars = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
 
 
 function generatePassword() {
-  var length = prompt("Select the length of the password");
+  var length = prompt("Select a length between 8 and 128.");
 
   // check if length is between 8 and 128 characters, also check if input isn't a valid number
-  if(length < 8 || length > 128 || isNaN(length)) {
+  while(length < 8 || length > 128 || isNaN(length)) {
     alert("Invalid length!");
-    generatePassword(); // recursive call until valid length is given
-    return;
+    length = prompt("Select a valid length between 8 and 128.");
+  }
+
+  var charList = [];
+  confirmCharacters();
+
+  var password = "";
+  for(var i = 0; i < length; i++) {
+    password = password.concat(charList[Math.floor(Math.random() * charList.length)]); // grab a random character from the list of available characters
+  }
+
+  return password;
+
+  // confirm what characters user wants to include: (lowercase, uppercase, numerics, special characters)
+  function confirmCharacters() {
+    var hasLowerCase = confirm("Do you want to include lowercase characters?");
+    var hasUpperCase = confirm("Do you want to include uppercase characters?");
+    var hasNumeric = confirm("Do you want to include numeric characters?");
+    var hasSpecialChar = confirm("Do you want to include special characters?");
+
+    if(!hasLowerCase && !hasUpperCase && !hasNumeric && !hasSpecialChar) {
+      alert("You have to pick at least one character type.");
+      confirmCharacters(); // recursive call until user has picked at least one type
+      return;
+    }
+
+    if(hasLowerCase) {
+      if(charList.length == 0) {
+        charList = lowerCaseChars;
+      }
+    }
+    if(hasUpperCase) {
+      if(charList.length == 0) {
+        charList = upperCaseChars;
+      }
+      else {
+        charList = charList.concat(upperCaseChars);
+      }
+    }
+    if(hasNumeric) {
+      if(charList.length == 0) {
+        charList = numericChars;
+      }
+      else {
+        charList = charList.concat(numericChars);
+      }
+    }
+    if(hasSpecialChar) {
+      if(charList.length == 0) {
+        charList = specialChars;
+      }
+      else {
+        charList = charList.concat(specialChars);
+      }
+    }
   }
 
 }
